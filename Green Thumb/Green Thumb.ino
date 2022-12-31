3537dd2ec60dc214e6efbb99675ebc4ecdbed1e0
Ryan Timmerman <ryantoddt@gmail.com>
2022-12-30 21:16:41 -0600

    Disabled the relays (by commenting out code) while I'm waiting on humidifier parts. Improved OLED output.

--- Green Thumb/Green Thumb.ino
+++ Green Thumb/Green Thumb.ino
@@ -7,35 +7,50 @@
 //***********************************************************************//
 
 #include <Wire.h>
-#include "ACROBOTIC_SSD1306.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "DHT.h"
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 #define DHTPIN A0
 #define DHTTYPE DHT11
-#define fanRelay 2
-#define humidifierRelay 4
+
+// #define fanRelay 2
+// #define humidifierRelay 4
 
 DHT dht(DHTPIN, DHTTYPE);
-int loopCtr = 0;
 
 void setup() {
 
-  Serial.begin(9600);
-  pinMode(fanRelay, OUTPUT);
-  pinMode(humidifierRelay, OUTPUT);
+  Serial.begin(115200);
+
+  // pinMode(fanRelay, OUTPUT);
+  // pinMode(humidifierRelay, OUTPUT);
 
   setupDisplay();
+
   dht.begin();  //initiate humid/temp sensor
 
-  initializeTestSequence();
+  // initializeTestSequence();
 }
 
 void setupDisplay() {
-  Wire.begin();
-  oled.init();           // Initialze SSD1306 OLED display
-  oled.clearDisplay();   // Clear screen
-  oled.setTextXY(0, 0);  // Set cursor position, start of line 0
-  oled.putString("Green Thumb 1.0");
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(0,0);
+  display.println("GreenThumb");
+  display.display();
+  display.setTextSize(2);
 }
 
 void loop() {
@@ -45,68 +60,61 @@ void loop() {
 
   updateDisplay(humid, tempC);
 
-  if (humid < 80) {
-    //turn fan and humidifer on for 5 seconds
-    Serial.println("LOW HUMIDITY: Powering on fan and humidifier..");
+  // if (humid < 80) {
+  //   //turn fan and humidifer on for 10 seconds
+  //   Serial.println("LOW HUMIDITY: Powering on fan and humidifier..");
 
-    digitalWrite(fanRelay, LOW);
-    digitalWrite(humidifierRelay, LOW);
+  //   digitalWrite(fanRelay, LOW);
+  //   digitalWrite(humidifierRelay, LOW);
 
-    delay(5000);
+  //   delay(10000);
 
-    digitalWrite(fanRelay, HIGH);         //turn relays off
-    digitalWrite(humidifierRelay, HIGH);  //turn humidifer relay off
-  }
+  //   digitalWrite(fanRelay, HIGH);         //turn relays off
+  //   digitalWrite(humidifierRelay, HIGH);  //turn humidifer relay off
+  // }
 
-  //every 4 hours turn the fan on for 10 seconds
-  if (loopCtr == 240) {
-    loopCtr = 0;
-    Serial.println("FRESH AIR: Powering on fan..");
-
-    digitalWrite(fanRelay, LOW);
-    delay(10000);
-    digitalWrite(fanRelay, HIGH);  //turn relays off
-  }
-
-  loopCtr++;
-
-  //wait 1 minute before looping
-  delay(60000);
+  //wait 10 sec before looping
+  delay(10000);
 }
 
-void initializeTestSequence() {
-  Serial.println("");
-  Serial.println("*************************************************");
-  Serial.println("Green Thumb 1.0 Starting..");
+// void initializeTestSequence() {
+//   Serial.println("");
+//   Serial.println("*************************************************");
+//   Serial.println("Green Thumb 1.0 Starting..");
 
-  //turn relays on for 3 seconds w/ .25s between relays
-  digitalWrite(fanRelay, LOW);
-  delay(250);
-  digitalWrite(humidifierRelay, LOW);
+//   //turn relays on for 3 seconds w/ .25s between relays
+//   digitalWrite(fanRelay, LOW);
+//   delay(250);
+//   digitalWrite(humidifierRelay, LOW);
 
-  delay(3000);  //wait 3 seconds
+//   delay(3000);  //wait 3 seconds
 
-  digitalWrite(fanRelay, HIGH);  //turn relays off
-  delay(250);
-  digitalWrite(humidifierRelay, HIGH);  //turn humidifer relay off
-}
+//   digitalWrite(fanRelay, HIGH);  //turn relays off
+//   delay(250);
+//   digitalWrite(humidifierRelay, HIGH);  //turn humidifer relay off
+// }
 
-void updateDisplay(float humidity, float tempC) {
-  String tempString = "Temp: " + String((float)(1.8 * tempC + 32)) + " F";
+void updateDisplay(int humidity, int tempC) {
+  String tempString = "TEMP " + String((int)(1.8 * tempC + 32)) + "F";
   int tempString_len = tempString.length() + 1;  // Length (with one extra character for the null terminator)
   char temp_char_array[tempString_len];
   tempString.toCharArray(temp_char_array, tempString_len);
 
-  String humidString = "Humidity: " + String(humidity) + "%";
+  String humidString = "RH: " + String(humidity) + "%";
   int humidString_len = humidString.length() + 1;  // Length (with one extra character for the null terminator)
   char humid_char_array[humidString_len];
   humidString.toCharArray(humid_char_array, humidString_len);
 
   Serial.println(tempString + "       " + humidString);
 
-  oled.setTextXY(3, 0);
-  oled.putString(temp_char_array);
+  display.setCursor(0,25);
+  display.println(temp_char_array);
+  display.setCursor(10,45);
+  display.println(humid_char_array);
+  display.invertDisplay(true);
+  display.display();
+  delay(1000);
 
-  oled.setTextXY(4, 0);
-  oled.putString(humid_char_array);
+  display.invertDisplay(false);
+  delay(1000);
 }
